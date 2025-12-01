6f834dd743ce1297cffcbde3442722be06ba795a
gone_ <gone_@DESKTOP-JTJ7KE8>
2025-11-30 21:20:34 -0500

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateofBirth = new DateTime(1984,12,14)
+            };
+            return View(data);
+        }
+    }
+}
